a3cbc712c622d15fbd2729291c1c31590aa49abb
Cong <congnguyen1006@gmail.com>
2015-12-11 07:55:33 +1030

    Updating database + Learning to improve map ui.

--- TW2-Data/MapView_R/ui.js
+++ TW2-Data/MapView_R/ui.js
@@ -1,32 +1,74 @@
 var map = L.map('map', {
     center: [0, 0],
     zoom: 8,
-    minZoom: 4.5
+    minZoom: 4.5,
+    maxZoom: 9
 });
 
 var geoLayer = L.geoJson().addTo(map);
 
 /* Styles */
 var cleanTile = {
+    weight: 0.25,
     color: "black",
-    opacity: 1,
-    weight: 0.5,
-    fillColor: "white"
+    fillColor: "white",
+    fillOpacity: 0.2
 };
+
+function activityColor(a) {
+    if(a > 7) return "#b10026";
+    if(a > 6) return "#e31a1c";
+    if(a > 5) return "#fc4e2a";
+    if(a > 4) return "#fd8d3c";
+    if(a > 3) return "#feb24c";
+    if(a > 2) return "#fed976";
+    if(a > 1) return "#ffeda0";
+    if(a > 0) return "#ffffcc";
+    if(a <= 0) return "#ffffff";
+}
+
+function style(feature) {
+    return {
+        fillColor: activityColor(feature.properties.awayDays),
+        weight: 2,
+        opacity: 1,
+        color: '#EEEEEE',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
 /* Load data */
 $.getJSON("./en15db/db/villagesGeoJSON.json", function(json) {
-    geoLayer.addData(json);
-    geoLayer.setStyle(cleanTile);
+    L.geoJson(json, {
+        style: cleanTile,
+        onEachFeature: onEachFeature
+    }).addTo(map);
     console.log("Ready");
 });
 
 /* Events */
-geoLayer.on("click", function(e) {
-    console.log(e.latlng);
-    console.log(e.id);
-    console.log("");
-});
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+}
 
-geoLayer.on("mouseover", function(e) {
-    //console.log(e.latlng);
-});
+function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 4,
+        color: "#666",
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    var layer = e.target;
+    layer.setStyle(cleanTile);
+}
